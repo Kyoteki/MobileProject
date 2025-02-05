6b3b5b50915f515e5850b04627405d8e87ab81e8
Decisy Gaëtan <gaetan.decisy6@hotmail.com>
2025-02-05 16:28:28 +0100

    Added first interactions

--- Assets/Scripts/Menu/ActivateButton.cs
+++ Assets/Scripts/Menu/ActivateButton.cs
@@ -1,9 +1,31 @@
+using System;
 using UnityEngine;
 
 public class ActivateButton : MonoBehaviour
 {
+    private MovementPlayer _mouvementPlayer;
+
+    void Awake()
+    {
+        _mouvementPlayer = GameManager.Instance.MovementPlayer;
+        
+        _mouvementPlayer.OnStartMove += Hide;
+        _mouvementPlayer.OnEndMove += Show;
+    }
+
     public void ClickButton()
     {
-        // GameManager.Instance.PlayerScript
+        CollisionManager.Instance.GetInteractableAt(_mouvementPlayer.transform.position).Interact();
+        Show();
+    }
+    
+    private void Show()
+    {
+        gameObject.SetActive(CollisionManager.Instance.InteractableAt(_mouvementPlayer.transform.position));
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
