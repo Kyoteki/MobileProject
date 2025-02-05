7dffb12d84a7d11e65bd253054d54abb855527a9
Tevadu52 <tevafoug@gmail.com>
2025-02-05 14:56:00 +0100

    [ADD] Colision Manager

---
+++ Assets/Scripts/ColisionManager.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class ColisionManager : MonoBehaviour
+{
+    public ColisionManager Instance {  get; private set; }
+    [SerializeField] LayerMask _layerInteractable;
+    [SerializeField] LayerMask _layerObstacle;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+    public IInteractable GetInteractableAt(Vector3 position)
+    {
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(position, Vector3.forward, 0.1f, _layerInteractable);
+        IInteractable result;
+        if (raycastHit2D.collider.TryGetComponent<IInteractable>(out result)) return result;
+        else throw new ArgumentNullException("Get Interactable Null");
+    }
+    public bool GetObstacleAt(Vector3 position)
+    {
+        return Physics2D.Raycast(position, Vector3.forward, 0.1f, _layerObstacle);
+    }
+}
