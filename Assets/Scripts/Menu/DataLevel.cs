b7cc8b6c817c03452f42ffb7dcb0adebe856520c
Mathieu Guitter <guittermathieu@gmail.com>
2025-02-07 13:06:35 +0100

    [ADD] DataLevelContainer

--- Assets/Scripts/Menu/DataLevel.cs
+++ Assets/Scripts/Menu/DataLevel.cs
@@ -1,17 +1,26 @@
+using UnityEditor;
 using UnityEngine;
 
-public class DataLevel : MonoBehaviour
+[System.Serializable]
+public class DataLevel
 {
+    private int _id;
     [SerializeField] private string _levelName;
     public string LevelName => _levelName;
-    [SerializeField] private UnityEngine.UI.Image _imagePreview;
-    public UnityEngine.UI.Image ImagePreview => _imagePreview;
-    [SerializeField] private int _nbStars = 0;
+    [SerializeField] private GameObject _prefab;
+    public GameObject Prefab => _prefab;
+    [SerializeField] private Sprite _imagePreviewMini;
+    public Sprite ImagePreviewMini => _imagePreviewMini;
+    [SerializeField] private Sprite _imagePreview;
+    public Sprite ImagePreview => _imagePreview;
+    private int _nbStars = 0;
     public int NbStars{ get => _nbStars; set => _nbStars = value; }
-    [SerializeField] private bool _isLocked = true;
-    public bool IsLocked{ get => _isLocked; set => _isLocked = value; }
-    [SerializeField] private bool _isCompleted = false;
+    private int _bestStep = 0;
+    public int BestStep{ get => _bestStep; set => _bestStep = value; }
+    private int _HighScore = 0;
+    public int HighScore{ get => _HighScore; set => _HighScore = value; }
+    private bool _isCompleted = false;
     public bool IsCompleted{ get => _isCompleted; set => _isCompleted = value; }
-    [SerializeField] private float _bestTime = 0.0f;
+    private float _bestTime = 0.0f;
     public float BestTime{ get => _bestTime; set => _bestTime = value; }
 }
